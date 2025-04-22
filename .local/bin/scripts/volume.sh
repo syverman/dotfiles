d290db4256d99e09dd5dc08baa3230faa7647407
syverman <syver29@gmail.com>
2025-04-22 19:50:23 -0400

    se actualizaron scripts

--- .local/bin/scripts/volume.sh
+++ .local/bin/scripts/volume.sh
@@ -1,83 +1,141 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-## Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
-##
-## Script to manage speaker volume on Archcraft.
+#  ▗▖  ▗▖ ▗▄▖ ▗▖   ▗▖ ▗▖▗▖  ▗▖▗▄▄▄▖     ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄▖▗▄▄▖  ▗▄▖ ▗▖   
+#  ▐▌  ▐▌▐▌ ▐▌▐▌   ▐▌ ▐▌▐▛▚▞▜▌▐▌       ▐▌   ▐▌ ▐▌▐▛▚▖▐▌  █  ▐▌ ▐▌▐▌ ▐▌▐▌   
+#  ▐▌  ▐▌▐▌ ▐▌▐▌   ▐▌ ▐▌▐▌  ▐▌▐▛▀▀▘    ▐▌   ▐▌ ▐▌▐▌ ▝▜▌  █  ▐▛▀▚▖▐▌ ▐▌▐▌   
+#   ▝▚▞▘ ▝▚▄▞▘▐▙▄▄▖▝▚▄▞▘▐▌  ▐▌▐▙▄▄▖    ▝▚▄▄▖▝▚▄▞▘▐▌  ▐▌  █  ▐▌ ▐▌▝▚▄▞▘▐▙▄▄▖
 
-# Icons
-iDIR='/home/juancarlos/.config/hypr/mako/icons/'
-notify_cmd='dunstify -u low -h string:x-dunst-stack-tag:obvolume'
+iDIR="$HOME/.config/swaync/icons"
+
 
 # Get Volume
 get_volume() {
-	echo "`pulsemixer --get-volume | cut -d' ' -f1`"
+    volume=$(pamixer --get-volume)
+    if [[ "$volume" -eq "0" ]]; then
+        echo "Muted"
+    else
+        echo "$volume%"
+    fi
 }
 
 # Get icons
 get_icon() {
-	current="$(get_volume)"
-	if [[ "$current" -eq "0" ]]; then
-		icon="$iDIR/volume-mute.png"
-	elif [[ ("$current" -ge "0") && ("$current" -le "30") ]]; then
-		icon="$iDIR/volume-low.png"
-	elif [[ ("$current" -ge "30") && ("$current" -le "60") ]]; then
-		icon="$iDIR/volume-mid.png"
-	elif [[ ("$current" -ge "60") && ("$current" -le "100") ]]; then
-		icon="$iDIR/volume-high.png"
-	fi
+    current=$(get_volume)
+    if [[ "$current" == "Muted" ]]; then
+        echo "$iDIR/volume-mute.png"
+    elif [[ "${current%\%}" -le 30 ]]; then
+        echo "$iDIR/volume-down.png"
+    elif [[ "${current%\%}" -le 60 ]]; then
+        echo "$iDIR/volume-mid.png"
+    else
+        echo "$iDIR/volume-up.png"
+    fi
 }
 
 # Notify
 notify_user() {
-	${notify_cmd} -i "$icon" "Volume : $(get_volume)%"
+    if [[ "$(get_volume)" == "Muted" ]]; then
+        notify-send -e -h string:x-canonical-private-synchronous:volume_notif -u low -i "$(get_icon)" "Volume: Muted"
+    else
+        notify-send -e -h int:value:"$(get_volume | sed 's/%//')" -h string:x-canonical-private-synchronous:volume_notif -u low -i "$(get_icon)" "Volume: $(get_volume)"
+    fi
 }
 
 # Increase Volume
 inc_volume() {
-	[[ `pulsemixer --get-mute` == 1 ]] && pulsemixer --unmute
-	pulsemixer --max-volume 100 --change-volume +5 && get_icon && notify_user
+    if [ "$(pamixer --get-mute)" == "true" ]; then
+        pamixer -u && notify_user
+    fi
+    pamixer -i 5 && notify_user
 }
 
 # Decrease Volume
 dec_volume() {
-	[[ `pulsemixer --get-mute` == 1 ]] && pulsemixer --unmute
-	pulsemixer --max-volume 100 --change-volume -5 && get_icon && notify_user
+    if [ "$(pamixer --get-mute)" == "true" ]; then
+        pamixer -u && notify_user
+    fi
+    pamixer -d 5 && notify_user
 }
 
 # Toggle Mute
 toggle_mute() {
-	if [[ `pulsemixer --get-mute` == 0 ]]; then
-		pulsemixer --toggle-mute && ${notify_cmd} -i "$iDIR/volume-mute.png" "Mute"
-	else
-		pulsemixer --toggle-mute && get_icon && ${notify_cmd} -i "$icon" "Unmute"
+	if [ "$(pamixer --get-mute)" == "false" ]; then
+		pamixer -m && notify-send -e -u low -i "$iDIR/volume-mute.png" "Sound OFF"
+	elif [ "$(pamixer --get-mute)" == "true" ]; then
+		pamixer -u && notify-send -e -u low -i "$(get_icon)" "Sound ON"
 	fi
 }
 
 # Toggle Mic
 toggle_mic() {
-	ID="`pulsemixer --list-sources | grep 'Default' | cut -d',' -f1 | cut -d' ' -f3`"
-	if [[ `pulsemixer --id $ID --get-mute` == 0 ]]; then
-		pulsemixer --id ${ID} --toggle-mute && ${notify_cmd} -i "$iDIR/microphone-mute.png" "Microphone Switched OFF"
-	else
-		pulsemixer --id ${ID} --toggle-mute && ${notify_cmd} -i "$iDIR/microphone.png" "Microphone Switched ON"
+	if [ "$(pamixer --default-source --get-mute)" == "false" ]; then
+		pamixer --default-source -m && notify-send -e -u low -i "$iDIR/microphone-mute.png" "Microphone OFF"
+	elif [ "$(pamixer --default-source --get-mute)" == "true" ]; then
+		pamixer -u --default-source u && notify-send -e -u low -i "$iDIR/microphone.png" "Microphone ON"
 	fi
+}
+# Get Mic Icon
+get_mic_icon() {
+    current=$(pamixer --default-source --get-volume)
+    if [[ "$current" -eq "0" ]]; then
+        echo "$iDIR/microphone-mute.png"
+    else
+        echo "$iDIR/microphone.png"
+    fi
+}
+
+# Get Microphone Volume
+get_mic_volume() {
+    volume=$(pamixer --default-source --get-volume)
+    if [[ "$volume" -eq "0" ]]; then
+        echo "Muted"
+    else
+        echo "$volume%"
+    fi
+}
+
+# Notify for Microphone
+notify_mic_user() {
+    volume=$(get_mic_volume)
+    icon=$(get_mic_icon)
+    notify-send -e -h int:value:"$volume" -h "string:x-canonical-private-synchronous:volume_notif" -u low -i "$icon" "Mic-Level: $volume"
+}
+
+# Increase MIC Volume
+inc_mic_volume() {
+    if [ "$(pamixer --default-source --get-mute)" == "true" ]; then
+        pamixer --default-source -u && notify_mic_user
+    fi
+    pamixer --default-source -i 5 && notify_mic_user
+}
+
+# Decrease MIC Volume
+dec_mic_volume() {
+    if [ "$(pamixer --default-source --get-mute)" == "true" ]; then
+        pamixer --default-source -u && notify_mic_user
+    fi
+    pamixer --default-source -d 5 && notify_mic_user
 }
 
 # Execute accordingly
-if [[ -x `which pulsemixer` ]]; then
-	if [[ "$1" == "--get" ]]; then
-		get_volume
-	elif [[ "$1" == "--inc" ]]; then
-		inc_volume
-	elif [[ "$1" == "--dec" ]]; then
-		dec_volume
-	elif [[ "$1" == "--toggle" ]]; then
-		toggle_mute
-	elif [[ "$1" == "--toggle-mic" ]]; then
-		toggle_mic
-	else
-		echo $(get_volume)%
-	fi
+if [[ "$1" == "--get" ]]; then
+	get_volume
+elif [[ "$1" == "--inc" ]]; then
+	inc_volume
+elif [[ "$1" == "--dec" ]]; then
+	dec_volume
+elif [[ "$1" == "--toggle" ]]; then
+	toggle_mute
+elif [[ "$1" == "--toggle-mic" ]]; then
+	toggle_mic
+elif [[ "$1" == "--get-icon" ]]; then
+	get_icon
+elif [[ "$1" == "--get-mic-icon" ]]; then
+	get_mic_icon
+elif [[ "$1" == "--mic-inc" ]]; then
+	inc_mic_volume
+elif [[ "$1" == "--mic-dec" ]]; then
+	dec_mic_volume
 else
-	${notify_cmd} "'pulsemixer' is not installed."
+	get_volume
 fi
